f9002aec6b71f2f4a348b69fc6cb53561ede343c
Habib-aw <88142412+Habib-aw@users.noreply.github.com>
2024-08-11 17:17:07 +0100

    menu bar

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ document.addEventListener("scroll", function () {
     logo.classList.remove("name-logo");
   }
 });
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
